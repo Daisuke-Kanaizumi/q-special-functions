764e8992c89fdd4fd3b68f03c0c9c1019e85bb50
Daisuke Kanaizumi <daisuke15aloha@gmail.com>
2017-09-16 17:17:24 +0900

    Add files via upload

---
+++ qPochhammerVer2.hpp
@@ -0,0 +1,83 @@
+// Author: Daisuke Kanaizumi
+// Affiliation: Department of Applied Mathematics, Waseda University
+// Email: daisuke15@asagi.waseda.jp
+
+// Verified computation of infinite q-Pochhammer symbol
+// This implementation is the second version
+// First Version:Pochhammer.hpp
+
+// Reference
+// Zhang, R. (2008). On asymptotics of q-Gamma functions. Journal of Mathematical Analysis and Applications, 339(2), 1313-1321.
+// Lemma 1.1
+#ifndef QPOCHHAMMERVER2_HPP
+#define QPOCHHAMMERVER2_HPP
+
+#include <kv/interval.hpp>
+#include <kv/rdouble.hpp>
+#include <kv/complex.hpp>
+#include <kv/constants.hpp>
+#include <kv/Heine.hpp>
+#include <kv/Pochhammer.hpp>
+#include <kv/Gatteschi.hpp>
+#include <cmath>
+
+namespace kv{
+  template <class T> complex<interval<T> >qPVer2(const complex<interval<T> >& z,const interval<T>& q, int n=100, int K=100){
+    complex<interval<T> >res,r,mid;
+    mid=0.;
+    T rad;
+    if (q>=1){
+      throw std::domain_error("value of q must be under 1");
+    }
+    if (q<=0){
+      throw std::domain_error("value of q must be positive");
+    }
+    if(q<0.9){
+      while(abs(z)*pow(q,n)/(1-q)>=0.5){
+	n=n+50;
+      }
+      rad=(2*pow(q,K*(K-1)*0.5)*pow(abs(z)*pow(q,n),K)
+	   /qPochhammer(interval<T>(q),interval<T>(q),int (K))).upper();
+      for(int k=0;k<=K-1;k++){
+	mid=mid+pow(q,k*(k-1)*0.5)*pow(-z*pow(q,n),k)
+	  /qPochhammer(interval<T>(q),interval<T>(q),int (k));
+      }
+      r=complex_nbd(mid,rad);
+      res=qPochhammer(complex<interval<T> >(z),interval<T>(q),int(n))*r;
+    }
+    else{
+      res=Gatteschi_qp(complex<interval<T> >(z),interval<T>(q));
+    }
+    return res;
+  }
+    template <class T> interval<T> qPVer2(const interval<T> & z,const interval<T>& q, int n=100, int K=100){
+    interval<T> res,r,mid;
+    mid=0.;
+    T rad;
+    if (q>=1){
+      throw std::domain_error("value of q must be under 1");
+    }
+    if (q<=0){
+      throw std::domain_error("value of q must be positive");
+    }
+    if(q<0.9){
+      while(abs(z)*pow(q,n)/(1-q)>=0.5){
+	n=n+50;
+      }
+      rad=(2*pow(q,K*(K-1)*0.5)*pow(abs(z)*pow(q,n),K)
+	   /qPochhammer(interval<T>(q),interval<T>(q),int (K))).upper();
+      for(int k=0;k<=K-1;k++){
+	mid=mid+pow(q,k*(k-1)*0.5)*pow(-z*pow(q,n),k)
+	  /qPochhammer(interval<T>(q),interval<T>(q),int (k));
+      }
+      r=mid+rad*interval<T>(-1.,1.);
+      res=qPochhammer(interval<T> (z),interval<T>(q),int(n))*r;
+    }
+    else{
+      res=Gatteschi_qp(interval<T> (z),interval<T>(q));
+    }
+    return res;
+}
+
+}
+#endif
