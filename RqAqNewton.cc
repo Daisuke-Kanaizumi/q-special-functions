634d2c3f37f3c9eb9ef9c0bbbeb383664bf09353
Daisuke Kanaizumi <daisuke15aloha@gmail.com>
2017-11-04 11:22:08 +0900

    Add files via upload

---
+++ RqAqNewton.cc
@@ -0,0 +1,21 @@
+#include <kv/Heine.hpp>
+#include <kv/qAiry.hpp>
+#include <cmath>
+typedef kv::interval<double> itv;
+typedef kv::complex< kv::interval<double> > cp;
+using namespace std;
+int main()
+{
+  cout.precision(17);
+  int n=50;
+  itv x,q;
+  q="0.7";
+ 
+  x=3.5;
+  for(int i=1;i<=n;i++){
+    x=x-kv::Ramanujan_qAiry(itv(q),itv(x))*(1-q)*x
+      /(kv::Ramanujan_qAiry(itv(q),itv(x))-kv::Ramanujan_qAiry(itv(q),itv(q*x)));
+  cout<<x<<endl;
+  cout<<"value of RqA"<<kv::Ramanujan_qAiry(itv(q),itv(x))<<endl;
+  }
+}
