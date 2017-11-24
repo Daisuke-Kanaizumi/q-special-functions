1e82fdf5a6f5d767a7d45a126a319d96fedca6b6
Daisuke Kanaizumi <daisuke15aloha@gmail.com>
2017-11-24 15:54:56 +0900

    Add files via upload

---
+++ qNewton/HEQintNewton.cc
@@ -0,0 +1,36 @@
+#include <kv/Heine.hpp>
+#include <kv/qAiry.hpp>
+#include <kv/qBessel.hpp>
+#include <cmath>
+#include <iostream>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <kv/interval.hpp>
+
+// define rounding operations for double
+// if "rdouble.hpp" is not included, result of computation is not "verified" 
+#include <kv/rdouble.hpp>
+typedef kv::interval<double> itv;
+typedef kv::complex< kv::interval<double> > cp;
+using namespace std;
+namespace ub = boost::numeric::ublas;
+int main()
+{
+  cout.precision(17);
+  ub::vector< itv > x(30);
+  int n=20;
+  itv nu,q,xx;
+  q="0.7";
+  nu=2.5;
+  x(0)=4.5;
+  
+  for(int i=1;i<=n;i++){
+    xx=mid(x(i-1));
+    x(i)=xx-kv::Hahn_Exton(itv(xx),itv(nu),itv(q))*(1-q)*x(i-1)
+      /(kv::Hahn_Exton(itv(x(i-1)),itv(nu),itv(q))-kv::Hahn_Exton(itv(q*x(i-1)),itv(nu),itv(q)));
+    
+    cout<<x(i)<<endl;
+    cout<<"value of HE"<<kv::Hahn_Exton(itv(x(i)),itv(nu),itv(q))<<endl;
+  }
+ 
+}
---
+++ qNewton/J2QintNewton.cc
@@ -0,0 +1,42 @@
+#include <kv/Heine.hpp>
+#include <kv/qAiry.hpp>
+#include <kv/qBessel.hpp>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <kv/interval.hpp>
+
+// define rounding operations for double
+// if "rdouble.hpp" is not included, result of computation is not "verified" 
+#include <kv/rdouble.hpp>
+typedef kv::interval<double> itv;
+typedef kv::complex< kv::interval<double> > cp;
+using namespace std;
+namespace ub = boost::numeric::ublas;
+
+int main()
+{
+  cout.precision(17);
+  ub::vector< itv > x(30);
+  int n=20;
+  itv y,nu,q,xx;
+  q="0.7";
+  nu=1.5;
+  y=30.;
+ 
+  x(0)=y;
+  
+  for(int i=1;i<=n;i++){
+    xx=mid(x(i-1));
+    x(i)=xx-kv::Jackson2(itv(xx),itv(nu),itv(q))*(1-q)*x(i-1)
+      /(kv::Jackson2(itv(x(i-1)),itv(nu),itv(q))-kv::Jackson2(itv(q*x(i-1)),itv(nu),itv(q)));
+    cout<<x(i)<<endl;
+    //cout<<i<<endl;
+    cout<<"value of J2"<<kv::Jackson2(itv(x(i)),itv(nu),itv(q))<<endl;
+  }
+ }
+  // to be updated
+  // A. L. Garcia, Numerical Methods for Physics, 2nd Edition, Pearson, 2015.
+
