abdb41617a0f01f0939ce967990823c013f3df9c
Daisuke Kanaizumi <daisuke15aloha@gmail.com>
2017-11-28 20:39:59 +0900

    Update qLpolyqNewton.cc

--- qNewton/qLpolyqNewton.cc
+++ qNewton/qLpolyqNewton.cc
@@ -9,17 +9,21 @@ int main()
 {
   cout.precision(17);
   int m=20;
-  int n=15;
+  int n=5;
   itv x,q,alpha;
   q="0.8";
-  n=15;
+  int nn;
+  nn=15;
   x=2.5;
   alpha=3.5;
-  for(int i=1;i<=n;i++){
+  for(int i=1;i<=nn;i++){
     x=x-kv::qLpoly(itv(x),itv(q),int(n),itv(alpha))*(1-q)*x
       /(kv::qLpoly(itv(x),itv(q),int(n),itv(alpha))-kv::qLpoly(itv(q*x),itv(q),int(n),itv(alpha)));
   cout<<x<<endl;
-  cout<<"value of qLpoly"<<kv::qLpoly(itv(x),itv(q),int(n),itv(alpha))<<endl;
+  cout<<"value of qLpoly inf"<<kv::qLpoly(itv(x.lower()),itv(q),int(n),itv(alpha))<<endl;
+  cout<<"value of qLpoly sup"<<kv::qLpoly(itv(x.upper()),itv(q),int(n),itv(alpha))<<endl;
+  cout<<"value of qLpoly mid"<<kv::qLpoly(itv(mid(x)),itv(q),int(n),itv(alpha))<<endl;
+
   }
 
 }
