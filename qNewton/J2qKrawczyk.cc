6f54c333abddd7bdbca83f65fec049b880dcba71
Daisuke Kanaizumi <daisuke15aloha@gmail.com>
2017-11-30 12:30:24 +0900

    Update J2qKrawczyk.cc

--- qNewton/J2qKrawczyk.cc
+++ qNewton/J2qKrawczyk.cc
@@ -2,26 +2,32 @@
 #include <kv/qAiry.hpp>
 #include <kv/qBessel.hpp>
 #include <cmath>
+#include <iostream>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
 typedef kv::interval<double> itv;
 typedef kv::complex< kv::interval<double> > cp;
 using namespace std;
+namespace ub = boost::numeric::ublas;
 int main()
 {
   cout.precision(17);
+  ub::vector< itv > x(30);
   int n=20;
-  itv x,nu,q,xx;
-  q="0.8";
+  itv nu,q,xx,xy;
+  q="0.5";
   nu=1.5;
-  x=2.5;
-  for(int i=1;i<=n;i++){
-    xx=mid(x);
-    x=xx-kv::Jackson2(itv(xx),itv(nu),itv(q))*(1-q)*xx
-      /(kv::Jackson2(itv(xx),itv(nu),itv(q))-kv::Jackson2(itv(q*xx),itv(nu),itv(q)))
-      +(1-xx*(kv::Jackson2(itv(x),itv(nu),itv(q))-kv::Jackson2(itv(q*x),itv(nu),itv(q))) 
-	/x/(kv::Jackson2(itv(xx),itv(nu),itv(q))-kv::Jackson2(itv(q*xx),itv(nu),itv(q))))*(x-xx);
-  cout<<x<<endl;
-  cout<<"value of J2 inf"<<kv::Jackson2(itv(x.lower()),itv(nu),itv(q))<<endl;
-  cout<<"value of J2 sup"<<kv::Jackson2(itv(x.upper()),itv(nu),itv(q))<<endl;
-  cout<<"value of J2 mid"<<kv::Jackson2(itv(mid(x)),itv(nu),itv(q))<<endl;
+  x(0)=5.;
+  xy=mid(x(0));
+  for(int i=0;i<=n;i++){
+    xx=mid(x(i));
+    x(i+1)=xx-kv::Jackson2(itv(xx),itv(nu),itv(q))*(1-q)*xy
+      /(kv::Jackson2(itv(xy),itv(nu),itv(q))-kv::Jackson2(itv(q*xy),itv(nu),itv(q)))
+      +(1-xy*(kv::Jackson2(itv(x(i)),itv(nu),itv(q))-kv::Jackson2(itv(q*x(i)),itv(nu),itv(q))) 
+	/x(i)/(kv::Jackson2(itv(xy),itv(nu),itv(q))-kv::Jackson2(itv(q*xy),itv(nu),itv(q))))*(x(i)-xx);
+    cout<<x(i+1)<<endl;
+    cout<<"value of J2 inf"<<kv::Jackson2(itv(x(i+1).lower()),itv(nu),itv(q))<<endl;
+    cout<<"value of J2 sup"<<kv::Jackson2(itv(x(i+1).upper()),itv(nu),itv(q))<<endl;
+    cout<<"value of J2 mid"<<kv::Jackson2(itv(mid(x(i+1))),itv(nu),itv(q))<<endl;
   }
 }
