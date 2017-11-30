b84b37e7775178baa3c70744f307b1612f83e260
Daisuke Kanaizumi <daisuke15aloha@gmail.com>
2017-11-30 12:37:21 +0900

    Update RqAqKrawczyk.cc

--- qNewton/RqAqKrawczyk.cc
+++ qNewton/RqAqKrawczyk.cc
@@ -1,27 +1,33 @@
 #include <kv/Heine.hpp>
 #include <kv/qAiry.hpp>
-
+#include <iostream>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
 #include <cmath>
 typedef kv::interval<double> itv;
 typedef kv::complex< kv::interval<double> > cp;
 using namespace std;
+namespace ub = boost::numeric::ublas;
 int main()
 {
   cout.precision(17);
-  int n=15;
-  itv x,q,xx;
+  ub::vector< itv > x(100);
+  int n=65;
+  itv q,xx,xy;
   q="0.9";
 
-  x=2.5;
-  for(int i=1;i<=n;i++){
-    xx=mid(x);
-    x=xx-kv::Ramanujan_qAiry(itv(q),itv(xx))*(1-q)*xx
-      /(kv::Ramanujan_qAiry(itv(q),itv(xx))-kv::Ramanujan_qAiry(itv(q),itv(q*xx)))
-      +(1-xx*(kv::Ramanujan_qAiry(itv(q),itv(x))-kv::Ramanujan_qAiry(itv(q),itv(q*x))) 
-	/x/(kv::Ramanujan_qAiry(itv(q),itv(xx))-kv::Ramanujan_qAiry(itv(q),itv(q*xx))))*(x-xx);
-  cout<<x<<endl;
-  cout<<"value of RqA inf"<<kv::Ramanujan_qAiry(itv(q),itv(x.lower()))<<endl;
-  cout<<"value of RqA sup"<<kv::Ramanujan_qAiry(itv(q),itv(x.upper()))<<endl;
-  cout<<"value of RqA mid"<<kv::Ramanujan_qAiry(itv(q),itv(mid(x)))<<endl;
+  x(0)=1.55;
+  xy=mid(x(0));
+  for(int i=0;i<=n;i++){
+    xx=mid(x(i));
+    x(i+1)=xx-kv::Ramanujan_qAiry(itv(q),itv(xx))*(1-q)*xy
+      /(kv::Ramanujan_qAiry(itv(q),itv(xy))-kv::Ramanujan_qAiry(itv(q),itv(q*xy)))
+      +(1-xy*(kv::Ramanujan_qAiry(itv(q),itv(x(i)))-kv::Ramanujan_qAiry(itv(q),itv(q*x(i)))) 
+	/x(i)/(kv::Ramanujan_qAiry(itv(q),itv(xy))-kv::Ramanujan_qAiry(itv(q),itv(q*xy))))*(x(i)-xx);
+    cout<<x(i+1)<<endl;
+    cout<<"value of RqA inf"<<kv::Ramanujan_qAiry(itv(q),itv(x(i+1).lower()))<<endl;
+    cout<<"value of RqA sup"<<kv::Ramanujan_qAiry(itv(q),itv(x(i+1).upper()))<<endl;
+    cout<<"value of RqA mid"<<kv::Ramanujan_qAiry(itv(q),itv(mid(x(i+1))))<<endl;
+
   }
 }
